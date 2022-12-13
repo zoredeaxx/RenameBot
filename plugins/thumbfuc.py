b2bc963c2a7248da64910df314e4b6da6d937629
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-12-13 19:32:42 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -14,7 +14,7 @@ async def viewthumb(client, message):
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("❌️ **Thumbnail Cleared Succesfully!!**")
+    await message.reply_text("❌️ **Thumbnail Cleared Successfully!!**")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
