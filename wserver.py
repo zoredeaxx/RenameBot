094d6628dc078d095c6907a36e60e6b0ce735454
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2024-01-26 21:38:17 +0530

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -1,1 +1,29 @@
+from flask import Flask
+from threading import Thread
+#import datetime
 
+
+app = Flask('')
+
+
+@app.route('/')
+
+def home():
+
+    #content = "<p>" + "Online @ " + str(datetime.datetime.now()) + "</p>"
+    #return content
+    return f"<h1>😎I'm Awake Already!🔥</h1>"
+
+def run():
+
+  app.run(host='0.0.0.0',port=8080)
+
+
+
+def keep_alive():  
+
+    t = Thread(target=run)
+
+    t.start()
+
+keep_alive()
